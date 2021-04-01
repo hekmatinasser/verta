18b988462893663b84f76bf40ded11553b09ddc4
alighorbani <alighorbani20002@gmail.com>
2021-04-01 22:54:30 +0430

    use one constant insted of two constant

--- src/VertaServiceProvider.php
+++ src/VertaServiceProvider.php
@@ -7,38 +7,25 @@ use Illuminate\Support\ServiceProvider;
 
 class VertaServiceProvider extends ServiceProvider
 {
-    const DATE_VALIDATORS = [
-        'jdate' => 'validateDate',
-        'jdate_equal' => 'validateDateEqual',
-        'jdate_not_equal' => 'validateDateNotEqual',
-        'jdatetime' => 'validateDateTime',
-        'jdatetime_equal' => 'validateDateTimeEqual',
-        'jdatetime_not_equal' => 'validateDateTimeNotEqual',
-        'jdate_after' => 'validateDateAfter',
-        'jdate_after_equal' => 'validateDateAfterEqual',
-        'jdatetime_after' => 'validateDateTimeAfter',
-        'jdatetime_after_equal' => 'validateDateTimeAfterEqual',
-        'jdate_before' => 'validateDateBefore',
-        'jdate_before_equal' => 'validateDateBeforeEqual',
-        'jdatetime_before' => 'validateDateTimeBefore',
-        'jdatetime_before_equal' => 'validateDateTimeBeforeEqual'
-    ];
+    const EXTEND_INDEX = 0;
 
-    const REPLACERS = [
-        'jdate' => 'replaceDateOrDatetime',
-        'jdate_equal' => 'replaceDateAfterOrBeforeOrEqual',
-        'jdate_not_equal' => 'replaceDateAfterOrBeforeOrEqual',
-        'jdatetime' => 'replaceDateOrDatetime',
-        'jdatetime_equal' => 'replaceDateTimeAfterOrBeforeOrEqual',
-        'jdatetime_not_equal' => 'replaceDateTimeAfterOrBeforeOrEqual',
-        'jdate_after' => 'replaceDateAfterOrBeforeOrEqual',
-        'jdate_after_equal' => 'replaceDateAfterOrBeforeOrEqual',
-        'jdatetime_after' => 'replaceDateTimeAfterOrBeforeOrEqual',
-        'jdatetime_after_equal' => 'replaceDateTimeAfterOrBeforeOrEqual',
-        'jdate_before' => 'replaceDateAfterOrBeforeOrEqual',
-        'jdate_before_equal' => 'replaceDateAfterOrBeforeOrEqual',
-        'jdatetime_before' => 'replaceDateTimeAfterOrBeforeOrEqual',
-        'jdatetime_before_equal' => 'replaceDateTimeAfterOrBeforeOrEqual',
+    const REPLACER_INDEX = 1;
+
+    const DATE_VALIDATORS = [
+        'jdate' => ['validateDate', 'replaceDateOrDatetime'],
+        'jdate_equal' => ['validateDateEqual', 'replaceDateAfterOrBeforeOrEqual'],
+        'jdate_not_equal' => ['validateDateNotEqual', 'replaceDateAfterOrBeforeOrEqual'],
+        'jdatetime' => ['validateDateTime', 'replaceDateOrDatetime'],
+        'jdatetime_equal' => ['validateDateTimeEqual', 'replaceDateTimeAfterOrBeforeOrEqual'],
+        'jdatetime_not_equal' => ['validateDateTimeNotEqual', 'replaceDateTimeAfterOrBeforeOrEqual'],
+        'jdate_after' => ['validateDateAfter', 'replaceDateAfterOrBeforeOrEqual'],
+        'jdate_after_equal' => ['validateDateAfterEqual', 'replaceDateAfterOrBeforeOrEqual'],
+        'jdatetime_after' => ['validateDateTimeAfter', 'replaceDateTimeAfterOrBeforeOrEqual'],
+        'jdatetime_after_equal' => ['validateDateTimeAfterEqual', 'replaceDateTimeAfterOrBeforeOrEqual'],
+        'jdate_before' => ['validateDateBefore', 'replaceDateAfterOrBeforeOrEqual'],
+        'jdate_before_equal' => ['validateDateBeforeEqual', 'replaceDateAfterOrBeforeOrEqual'],
+        'jdatetime_before' => ['validateDateTimeBefore', 'replaceDateTimeAfterOrBeforeOrEqual'],
+        'jdatetime_before_equal' => ['validateDateTimeBeforeEqual', 'replaceDateTimeAfterOrBeforeOrEqual'],
     ];
 
     /**
@@ -77,11 +64,12 @@ class VertaServiceProvider extends ServiceProvider
     protected function validator()
     {
         foreach (self::DATE_VALIDATORS as $name => $method) {
-            Validator::extend($name, JalaliValidator::class . '@' . $method);
-        }
 
-        foreach (self::REPLACERS as $name => $method) {
-            Validator::replacer($name, JalaliValidator::class . '@' . $method);
+            $validatorClass = JalaliValidator::class . '@';
+
+            Validator::extend($name, $validatorClass . $method[self::EXTEND_INDEX]);
+            
+            Validator::replacer($name, $validatorClass . $method[self::REPLACER_INDEX]);
         }
     }
 }
