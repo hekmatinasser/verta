3e8790d65197b8e6dd005a14d2f15d7f46c470f0
nasser <hekmati.nasser@gmail.com>
2021-04-02 00:53:26 +0430

    fix parse function

--- src/Traits/Creator.php
+++ src/Traits/Creator.php
@@ -148,10 +148,8 @@ trait Creator
         }, range(1,12));
         $formatted = str_replace($names, $values, $datetime);
         $formatted = str_replace(self::$monthYear, range(1,12), $formatted);
-
         $parse = date_parse($formatted);
-
-        if($parse['error_count'] > 0 && !self::isValidDate($parse['year'], $parse['month'], $parse['day']) && !self::isValidTime($parse['hour'], $parse['minute'], $parse['second'])) {
+        if($parse['error_count'] > 0 || !self::isValidDate($parse['year'], $parse['month'], $parse['day']) || !self::isValidTime($parse['hour'], $parse['minute'], $parse['second'])) {
             throw new InvalidArgumentException(sprintf("Unknown datetime '%s'", $datetime));
         }
 
@@ -174,7 +172,7 @@ trait Creator
         $formatted = str_replace(self::$monthYear, range(1,12), $datetime);
 
         $parse = date_parse_from_format($format, $formatted);
-        if($parse['error_count'] > 0 && !self::isValidDate($parse['year'], $parse['month'], $parse['day']) && !self::isValidTime($parse['hour'], $parse['minute'], $parse['second'])) {
+        if($parse['error_count'] > 0 || !self::isValidDate($parse['year'], $parse['month'], $parse['day']) || !self::isValidTime($parse['hour'], $parse['minute'], $parse['second'])) {
             throw new InvalidArgumentException(sprintf("Unknown datetime '%s'", $datetime));
         }
         list($year, $month, $day) = self::getGregorian($parse['year'], $parse['month'], $parse['day']);
