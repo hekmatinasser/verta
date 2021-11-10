fe6b056b52920d2a4e83d4ee98e63e0c10d95f28
Ehsan Sabet <ehsan.sabet@hotmail.com>
2021-11-10 12:11:00 +0330

    add quarter format

--- src/Lang/ps.php
+++ src/Lang/ps.php
@@ -71,6 +71,12 @@ return [
         'jdate_before_equal' => ':attribute' . ' تاریخ شمسی باید قبل یا برابر از ' . ':date' . ' باشد.',
         'jdatetime_before' => ':attribute' . ' تاریخ و زمان شمسی باید قبل از ' . ':date' . ' باشد.',
         'jdatetime_before_equal' => ':attribute' . ' تاریخ و زمان شمسی باید قبل یا برابر از ' . ':date' . ' باشد.',
+    ],
+    'quarters' => [
+        '1' => 'بهار',
+        '2' => 'تابستان',
+        '3' => 'پاییز',
+        '4' => 'زمستان',
     ]
 ];
 
