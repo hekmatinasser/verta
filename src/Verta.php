523bdc180b873600d543c6cb7f0f53bb945f569f
nasser <hekmati.nasser@gmail.com>
2022-08-20 23:32:07 +0430

    add convert carbon

--- src/Verta.php
+++ src/Verta.php
@@ -2,8 +2,21 @@
 
 namespace Hekmatinasser\Verta;
 
+use Exception;
 use Hekmatinasser\Jalali\Jalali;
+use Illuminate\Support\Carbon;
 
 class Verta extends Jalali
 {
+
+    /**
+     * Create a Carbon instance from Verta
+     *
+     * @return Carbon
+     * @throws Exception
+     */
+    public function toCarbon(): Carbon
+    {
+        return new Carbon($this->datetime(), $this->timezone);
+    }
 }
